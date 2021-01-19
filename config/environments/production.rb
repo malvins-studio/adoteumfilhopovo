4f299bb024523dbd2c51204b78bf0562676ca422
Michael Lins <malvins@gmail.com>
2021-01-19 16:46:02 -0300

    add mailer asset host url to fix images not showing on email body

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   config.action_controller.asset_host = 'https://adoteumpovo.org.br'
+  config.action_mailer.asset_host = config.action_controller.asset_host
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
