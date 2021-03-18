04d7e95ce50a9262a2915283c73f34d6d6a38e99
Michael Lins <michael@lins>
2021-03-17 21:30:20 -0300

    New logo replacement, started changing povo->filho povo, notification alert moved nearby form submit button

--- app/mailers/adocao_mailer.rb
+++ app/mailers/adocao_mailer.rb
@@ -3,6 +3,6 @@ class AdocaoMailer < ApplicationMailer
     @adotante = adocao.adotante
     @povo = adocao.pna
     attachments['cartao_filho_povo.png'] = File.read("#{Rails.root}/app/assets/images/prayer_cards/carta_povo_#{@povo.id}.png")
-    mail(to: @adotante.email, subject: "Seu filho povo chegou! Cuide bem dele!", from: "Adote um povo<contato@adoteumpovo.org.br>")
+    mail(to: @adotante.email, subject: "Seu filho povo chegou! Cuide bem dele!", from: "Adote um filho povo<contato@adoteumfilhopovo.org.br>")
   end
 end
