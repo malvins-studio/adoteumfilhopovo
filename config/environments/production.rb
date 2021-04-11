e17ad1d0e65674629cfa4a580e6877412d98bba6
Michael Lins <michael@devsquad.email>
2021-04-10 21:46:29 -0300

    fix: filhopovo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "adoteumpovo_org_br_production"
+  # config.active_job.queue_name_prefix = " adoteumfilhopovo_production"
 
   config.action_mailer.perform_caching = false
 
