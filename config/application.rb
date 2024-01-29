7755e3ffe70fa6c39c5e23a1d2b9492fb5a64ba0
Michael Lins <michael@malvins.studio>
2024-01-29 14:24:10 -0300

    fix: update default version to 7 to handle cache properly

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module AdoteUmFilhoPovo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     config.i18n.default_locale = 'pt-BR'
 
